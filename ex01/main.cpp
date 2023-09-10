a0d6cab22756b1979f65e5c869863f13faac7dd9
Stephanie Lakner <stephanie.lakner@rub.de>
2023-09-10 16:31:29 +0200

    Re-write this with stack and re-read how RPN works

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,11 +1,11 @@
 #include "RPN.hpp"
-#include <array>
+#include <stack>
 #include <iostream>
+
+// I have to re-write this whole thing using a stack
 
 int main(int argc, char **argv)
 {
-	std::array<char, 21> expression;
-
 	if (argc != 2)
 	{
 		std::cout << "Wrong number of arguments" << std::endl;
@@ -14,44 +14,56 @@ int main(int argc, char **argv)
 
 	std::string s_expr = argv[1];
 
-	size_t j = 0;
-	for (size_t i = 0; i < s_expr.length(); i++)
-	{
-		if (isspace(s_expr[i]))
-			continue;
-		expression[j] = s_expr[i];
-		j++;
-	}
-	expression[j] = 0;
-	std::cout << "j: " << j << std::endl;
-
-	if (!(j % 2) || !validate(expression))
+	s_expr.erase(remove_if(s_expr.begin(), s_expr.end(), isspace), s_expr.end());
+	if (!isdigit(s_expr[0]))
 	{
 		std::cout << "Error" << std::endl;
-		return(1);
+		return 1;
 	}
-	double result = expression[0] - '0';
-	for (int i = 1; expression[i]; i+=2)
+	std::stack<double> rpnstack;
+	//for (int i = 0; s_expr[i]; i++)
+	double result = (s_expr[0] - '0');
+	for (std::string::iterator it = (s_expr.begin()) + 1; it != s_expr.end(); it++)
 	{
-		std::cout << result << std::endl;
-		switch (expression[i + 1])
+		// std::cout << *it << std::endl;
+		if (isdigit(*it))
 		{
-			case '+':
-				result += expression[i] - '0';
-				break;
-			case '-':
-				result -= expression[i] - '0';
-				break;
-			case '*':
-				result *= expression[i] - '0';
-				break;
-			case '/':
-				result /= expression[i] - '0';
-				break;
-			default:
-				std::cout << "Error" << std::endl;
+			rpnstack.push(*it - '0');
+		}
+		else
+		{
+			std::string operands = "+-*/";
+			// std::cout << *it << std::endl;
+			if (operands.find(*it) == std::string::npos)
+			{
+				std::cout << "Error: invalid character: " << *it << std::endl;
 				return 1;
+			}
+			switch (*it)
+			{
+				result = rpnstack.top();
+				rpnstack.pop();
+				case '+':
+					result += rpnstack.top();
+					break;
+				case '-':
+					result -= rpnstack.top();
+					break;
+				case '*':
+					result *= rpnstack.top();
+					break;
+				case '/':
+					result /= rpnstack.top();
+					break;
+				default:
+					std::cout << "Error" << std::endl;
+					return 1;
+			}
+			rpnstack.pop();
+			rpnstack.push(result);
+			// std::cout << "Result: " << result << std::endl;
 		}
 	}
+	std::cout << result << std::endl;
 	return 0;
 }
