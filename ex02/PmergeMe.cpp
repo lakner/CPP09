5fd5679848aefd8c13f6a5a02040c7d51cdce259
Stephanie Lakner <stephanie.lakner@rub.de>
2023-09-28 21:15:26 +0200

    It compiles

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,6 +1,7 @@
 #include "PmergeMe.hpp"
 #include <iostream>
 #include <ctime>
+#include <iterator>
 
 PmergeMe::PmergeMe()
 {
@@ -54,8 +55,7 @@ void PmergeMe::printList()
 double PmergeMe::sortVec()
 {
 	clock_t start = clock();
-
-	
+	sortVec2(this->_vec);	
 	clock_t end = clock();
 	return(static_cast<double> (end - start)/CLOCKS_PER_SEC * 1000000);
 }
@@ -63,6 +63,62 @@ double PmergeMe::sortVec()
 double PmergeMe::sortList()
 {
 	clock_t start = clock();
+	sortList2(this->_list);
 	clock_t end = clock();
 	return(static_cast<double> (end - start)/CLOCKS_PER_SEC * 1000000);
+}
+
+std::vector<unsigned int> PmergeMe::sortVec2(std::vector<unsigned int> seq)
+{
+	if (std::is_sorted(seq.begin(), seq.end()))
+		return (seq);
+	std::vector<unsigned int> larger_elems;
+	std::vector<unsigned int>::iterator it_smallest = seq.begin();
+	for(std::vector<unsigned int>::iterator it = seq.begin(); it != seq.end(); std::advance(it, 2))
+	{
+		if (*it < *(next(it)))
+		{
+			if (*it < *it_smallest)
+				it_smallest = it;
+			larger_elems.push_back(*(next(it)));
+		}
+		else
+		{
+			if (*(next(it)) < *it_smallest)
+				it_smallest = next(it);
+			larger_elems.push_back(*it);
+		}
+	}
+	larger_elems = sortVec2(larger_elems);
+	larger_elems.insert(larger_elems.begin(), *it_smallest);
+	seq.erase(it_smallest);
+	return(larger_elems);
+}
+
+std::list<unsigned int> PmergeMe::sortList2(std::list<unsigned int> seq)
+{
+	if (std::is_sorted(seq.begin(), seq.end()))
+		return (seq);
+	std::list<unsigned int> larger_elems;
+	std::list<unsigned int>::iterator it_smallest = seq.begin();
+	for(std::list<unsigned int>::iterator it = seq.begin(); it != seq.end(); std::advance(it, 2))
+	{
+		if (*it < *(next(it)))
+		{
+			if (*it < *it_smallest)
+				it_smallest = it;
+			larger_elems.push_back(*(next(it)));
+		}
+		else
+		{
+			if (*(next(it)) < *it_smallest)
+				it_smallest = next(it);
+			larger_elems.push_back(*it);
+		}
+	}
+	larger_elems = sortList2(larger_elems);
+	larger_elems.insert(larger_elems.begin(), *it_smallest);
+	seq.erase(it_smallest);
+
+	return(larger_elems);
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -4,6 +4,7 @@
 #pragma once
 #include <vector>
 #include <list>
+#include <iterator>
 
 class PmergeMe
 {
@@ -20,9 +21,16 @@ class PmergeMe
 		double sortVec();
 
 	private:
+		std::list<unsigned int> sortList2(std::list<unsigned int>);
+		std::vector<unsigned int> sortVec2(std::vector<unsigned int>);
 		std::vector<unsigned int> _vec;
 		std::list<unsigned int> _list;
-
+		template<class T>
+		T next(T it, typename std::iterator_traits<T>::difference_type n = 1)
+		{
+			std::advance(it, n);
+			return (it);
+		};
 };
 
 #endif
